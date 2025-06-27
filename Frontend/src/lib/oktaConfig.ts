f77971d109b975e2355fff53bd2f485751410086
Atharva Mahajan <157297405+Atharva-Mahajan-1104@users.noreply.github.com>
2025-06-27 21:15:19 +0530

    deleted config files

--- Frontend/src/lib/oktaConfig.ts
+++
@@ -1,9 +0,0 @@
-export const oktaConfig={
-clientId:'0oaiuljnvw3TkB4Rc5d7',
-issuer:'https://dev-32843828.okta.com/oauth2/default',
-redirectUri:'http://localhost:3000/login/callback',
-scopes:['openid','profile','email'],
-pkce:true,
-disableHttpsCheck:true
-
-}
